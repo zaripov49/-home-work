0a34e63c7ee0da626e124d3eaf94be871dda83f0
zaripov49 <zaripovalisher.49@gmail.com>
2025-01-09 16:13:02 +0300

    all add

---
+++ lessons1/Task 10/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr;
+int n = Convert.ToInt32(Console.ReadLine());
+arr = new int[n];
+for (int i = 0; i < n; i++)
+{
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+// int sum=0;
+for (int i =0; i<n; i++)
+{
+    if(arr[i]%2!=0)
+    {
+        Console.WriteLine(Math.Pow(arr[i],2));
+    }
+}
